9ad220111f701046184db7613f6798d52c1b4789
Atharv Vohra <atharv@ualberta.ca>
2019-01-26 13:38:38 -0700

    enemy movement?

--- src/Scripts/ConstructionWorker.gd
+++ src/Scripts/ConstructionWorker.gd
@@ -4,12 +4,17 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 onready var playerNode = get_node("Player")
+var movedir 
+var playerPosition
+var playerDistance
+var velocity
 var MOVE_SPEED = 300
-
+	
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
+
 
 func _process(delta):
 	playerPosition = get_node("Player").position
@@ -23,17 +28,12 @@ func _process(delta):
 		
 func attack():
 	playerNode.health -= 20
-	
+	# play the animation and sound effect stuff
 	
 func move():
-	var motion = movedir.normalized() * MOTION_SPEED
-	var collision = move_and_collide(motion*delta)
-	if collision:
-		move_and_slide(motion)
-	if movedir == Vector2():
-		anim = "Idle"
-	if anim != animNew:
-		animNew = anim
-		#AnimNode.play(anim)
-	
+	# get the direction, move and slide
+	velocity = (playerPosition - position).normalized() * MOVE_SPEED
+    # rotation = velocity.angle()
+	if (playerPosition - position).length() > 5:
+    	move_and_slide(velocity)
 	
