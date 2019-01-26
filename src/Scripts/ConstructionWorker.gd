13ff5b5a714bcfeda30566598e942a6f50c66197
Atharv Vohra <atharv@ualberta.ca>
2019-01-26 13:53:41 -0700

    enemy movement v2

--- src/Scripts/ConstructionWorker.gd
+++ src/Scripts/ConstructionWorker.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-onready var playerNode = get_node("Player")
+onready var playerNode = get_tree().root.get_node("World/Player")
 var movedir 
 var playerPosition
 var playerDistance
@@ -17,11 +17,11 @@ func _ready():
 
 
 func _process(delta):
-	playerPosition = get_node("Player").position
+	playerPosition = playerNode.position
 	 
 	playerDistance = position.distance_to(playerPosition)
 	# if distance < 20, attack
-	if(playerDistance < 5):
+	if(playerDistance < 64):
 		attack()
 	else:
 		move()
@@ -34,6 +34,6 @@ func move():
 	# get the direction, move and slide
 	velocity = (playerPosition - position).normalized() * MOVE_SPEED
     # rotation = velocity.angle()
-	if (playerPosition - position).length() > 5:
+	if (playerPosition - position).length() > 100:
     	move_and_slide(velocity)
 	
