24d83eec743a4a20649a4e68e28d7e2cf30f7d3a
Rishi Barnwal <barnwal.rishi@gmail.com>
2019-01-27 12:53:25 -0700

    UI DESIGN BINTCHES!!!!

---
+++ src/Scenes/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("ui_light_attack"):
+		get_tree().change_scene("res://Scenes/World.tscn")
